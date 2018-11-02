61afa3eae125e5c8c170a231769f2e793af8361b
unknown <guchiyuan@gtmapdc.cn>
2018-11-02 17:27:39 +0800

    init headerview navi

--- my-project/src/components/HeaderView.vue
+++ my-project/src/components/HeaderView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="headerview">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">简历制作</el-menu-item>
-      <el-menu-item index="2">关于我</el-menu-item>
+    <el-menu :default-active="$route.path" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/">简历制作</el-menu-item>
+      <el-menu-item index="/about">关于我</el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -12,14 +12,15 @@ export default {
   name: 'headerview',
   data() {
     return {
-      activeIndex: '2',
+      // activeIndex: '2',
     }
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        this.$router.push(key);
+        console.log(key, keyPath);
+      }
     }
-  }
 }
 </script>
 
