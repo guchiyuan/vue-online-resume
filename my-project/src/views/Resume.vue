2ba128045574f337535051de5ef16e07fb481858
guchiyuan <chiyuan.gu1991@gmail.com>
2018-11-04 23:31:20 +0800

    20181103

--- my-project/src/views/Resume.vue
+++ my-project/src/views/Resume.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="resume">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <el-container>
+      <el-aside class="menu-panel" width="300px">
+        <el-menu mode="verticle" collapse="false" default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <!-- <span slot="title">导航一</span> -->
+            </template>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="content-panel">Main</el-main>
+    </el-container>
   </div>
-</template>
+</template> 
 
 <script>
 
@@ -13,3 +25,14 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .menu-panel
+    margin .3rem
+    background-color #eee
+    height 600px
+  .content-panel
+    background-color #666
+    margin .3rem
+</style>
+
